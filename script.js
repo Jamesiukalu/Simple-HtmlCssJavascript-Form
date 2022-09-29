2fb23ef0d5ada327d28f8222b7bc38e98254da76
jamesiukalu <jamesiukalu@gmail.com>
2022-09-29 16:12:15 +0100

    Simple-HtmlCssJavascript-Form

---
+++ script.js
@@ -0,0 +1,369 @@
+// include array
+// All countries
+// length 252
+/* var countries = {
+    "AF": "Afghanistan",
+    "AX": "Aland Islands",
+    "AL": "Albania",
+    "DZ": "Algeria",
+    "AS": "American Samoa",
+    "AD": "Andorra",
+    "AO": "Angola",
+    "AI": "Anguilla",
+    "AQ": "Antarctica",
+    "AG": "Antigua and Barbuda",
+    "AR": "Argentina",
+    "AM": "Armenia",
+    "AW": "Aruba",
+    "AU": "Australia",
+    "AT": "Austria",
+    "AZ": "Azerbaijan",
+    "BS": "Bahamas",
+    "BH": "Bahrain",
+    "BD": "Bangladesh",
+    "BB": "Barbados",
+    "BY": "Belarus",
+    "BE": "Belgium",
+    "BZ": "Belize",
+    "BJ": "Benin",
+    "BM": "Bermuda",
+    "BT": "Bhutan",
+    "BO": "Bolivia",
+    "BQ": "Bonaire, Sint Eustatius and Saba",
+    "BA": "Bosnia and Herzegovina",
+    "BW": "Botswana",
+    "BV": "Bouvet Island",
+    "BR": "Brazil",
+    "IO": "British Indian Ocean Territory",
+    "BN": "Brunei Darussalam",
+    "BG": "Bulgaria",
+    "BF": "Burkina Faso",
+    "BI": "Burundi",
+    "KH": "Cambodia",
+    "CM": "Cameroon",
+    "CA": "Canada",
+    "CV": "Cape Verde",
+    "KY": "Cayman Islands",
+    "CF": "Central African Republic",
+    "TD": "Chad",
+    "CL": "Chile",
+    "CN": "China",
+    "CX": "Christmas Island",
+    "CC": "Cocos (Keeling) Islands",
+    "CO": "Colombia",
+    "KM": "Comoros",
+    "CG": "Congo",
+    "CD": "Congo, Democratic Republic of the Congo",
+    "CK": "Cook Islands",
+    "CR": "Costa Rica",
+    "CI": "Cote D'Ivoire",
+    "HR": "Croatia",
+    "CU": "Cuba",
+    "CW": "Curacao",
+    "CY": "Cyprus",
+    "CZ": "Czech Republic",
+    "DK": "Denmark",
+    "DJ": "Djibouti",
+    "DM": "Dominica",
+    "DO": "Dominican Republic",
+    "EC": "Ecuador",
+    "EG": "Egypt",
+    "SV": "El Salvador",
+    "GQ": "Equatorial Guinea",
+    "ER": "Eritrea",
+    "EE": "Estonia",
+    "ET": "Ethiopia",
+    "FK": "Falkland Islands (Malvinas)",
+    "FO": "Faroe Islands",
+    "FJ": "Fiji",
+    "FI": "Finland",
+    "FR": "France",
+    "GF": "French Guiana",
+    "PF": "French Polynesia",
+    "TF": "French Southern Territories",
+    "GA": "Gabon",
+    "GM": "Gambia",
+    "GE": "Georgia",
+    "DE": "Germany",
+    "GH": "Ghana",
+    "GI": "Gibraltar",
+    "GR": "Greece",
+    "GL": "Greenland",
+    "GD": "Grenada",
+    "GP": "Guadeloupe",
+    "GU": "Guam",
+    "GT": "Guatemala",
+    "GG": "Guernsey",
+    "GN": "Guinea",
+    "GW": "Guinea-Bissau",
+    "GY": "Guyana",
+    "HT": "Haiti",
+    "HM": "Heard Island and Mcdonald Islands",
+    "VA": "Holy See (Vatican City State)",
+    "HN": "Honduras",
+    "HK": "Hong Kong",
+    "HU": "Hungary",
+    "IS": "Iceland",
+    "IN": "India",
+    "ID": "Indonesia",
+    "IR": "Iran, Islamic Republic of",
+    "IQ": "Iraq",
+    "IE": "Ireland",
+    "IM": "Isle of Man",
+    "IL": "Israel",
+    "IT": "Italy",
+    "JM": "Jamaica",
+    "JP": "Japan",
+    "JE": "Jersey",
+    "JO": "Jordan",
+    "KZ": "Kazakhstan",
+    "KE": "Kenya",
+    "KI": "Kiribati",
+    "KP": "Korea, Democratic People's Republic of",
+    "KR": "Korea, Republic of",
+    "XK": "Kosovo",
+    "KW": "Kuwait",
+    "KG": "Kyrgyzstan",
+    "LA": "Lao People's Democratic Republic",
+    "LV": "Latvia",
+    "LB": "Lebanon",
+    "LS": "Lesotho",
+    "LR": "Liberia",
+    "LY": "Libyan Arab Jamahiriya",
+    "LI": "Liechtenstein",
+    "LT": "Lithuania",
+    "LU": "Luxembourg",
+    "MO": "Macao",
+    "MK": "Macedonia, the Former Yugoslav Republic of",
+    "MG": "Madagascar",
+    "MW": "Malawi",
+    "MY": "Malaysia",
+    "MV": "Maldives",
+    "ML": "Mali",
+    "MT": "Malta",
+    "MH": "Marshall Islands",
+    "MQ": "Martinique",
+    "MR": "Mauritania",
+    "MU": "Mauritius",
+    "YT": "Mayotte",
+    "MX": "Mexico",
+    "FM": "Micronesia, Federated States of",
+    "MD": "Moldova, Republic of",
+    "MC": "Monaco",
+    "MN": "Mongolia",
+    "ME": "Montenegro",
+    "MS": "Montserrat",
+    "MA": "Morocco",
+    "MZ": "Mozambique",
+    "MM": "Myanmar",
+    "NA": "Namibia",
+    "NR": "Nauru",
+    "NP": "Nepal",
+    "NL": "Netherlands",
+    "AN": "Netherlands Antilles",
+    "NC": "New Caledonia",
+    "NZ": "New Zealand",
+    "NI": "Nicaragua",
+    "NE": "Niger",
+    "NG": "Nigeria",
+    "NU": "Niue",
+    "NF": "Norfolk Island",
+    "MP": "Northern Mariana Islands",
+    "NO": "Norway",
+    "OM": "Oman",
+    "PK": "Pakistan",
+    "PW": "Palau",
+    "PS": "Palestinian Territory, Occupied",
+    "PA": "Panama",
+    "PG": "Papua New Guinea",
+    "PY": "Paraguay",
+    "PE": "Peru",
+    "PH": "Philippines",
+    "PN": "Pitcairn",
+    "PL": "Poland",
+    "PT": "Portugal",
+    "PR": "Puerto Rico",
+    "QA": "Qatar",
+    "RE": "Reunion",
+    "RO": "Romania",
+    "RU": "Russian Federation",
+    "RW": "Rwanda",
+    "BL": "Saint Barthelemy",
+    "SH": "Saint Helena",
+    "KN": "Saint Kitts and Nevis",
+    "LC": "Saint Lucia",
+    "MF": "Saint Martin",
+    "PM": "Saint Pierre and Miquelon",
+    "VC": "Saint Vincent and the Grenadines",
+    "WS": "Samoa",
+    "SM": "San Marino",
+    "ST": "Sao Tome and Principe",
+    "SA": "Saudi Arabia",
+    "SN": "Senegal",
+    "RS": "Serbia",
+    "CS": "Serbia and Montenegro",
+    "SC": "Seychelles",
+    "SL": "Sierra Leone",
+    "SG": "Singapore",
+    "SX": "Sint Maarten",
+    "SK": "Slovakia",
+    "SI": "Slovenia",
+    "SB": "Solomon Islands",
+    "SO": "Somalia",
+    "ZA": "South Africa",
+    "GS": "South Georgia and the South Sandwich Islands",
+    "SS": "South Sudan",
+    "ES": "Spain",
+    "LK": "Sri Lanka",
+    "SD": "Sudan",
+    "SR": "Suriname",
+    "SJ": "Svalbard and Jan Mayen",
+    "SZ": "Swaziland",
+    "SE": "Sweden",
+    "CH": "Switzerland",
+    "SY": "Syrian Arab Republic",
+    "TW": "Taiwan, Province of China",
+    "TJ": "Tajikistan",
+    "TZ": "Tanzania, United Republic of",
+    "TH": "Thailand",
+    "TL": "Timor-Leste",
+    "TG": "Togo",
+    "TK": "Tokelau",
+    "TO": "Tonga",
+    "TT": "Trinidad and Tobago",
+    "TN": "Tunisia",
+    "TR": "Turkey",
+    "TM": "Turkmenistan",
+    "TC": "Turks and Caicos Islands",
+    "TV": "Tuvalu",
+    "UG": "Uganda",
+    "UA": "Ukraine",
+    "AE": "United Arab Emirates",
+    "GB": "United Kingdom",
+    "US": "United States",
+    "UM": "United States Minor Outlying Islands",
+    "UY": "Uruguay",
+    "UZ": "Uzbekistan",
+    "VU": "Vanuatu",
+    "VE": "Venezuela",
+    "VN": "Viet Nam",
+    "VG": "Virgin Islands, British",
+    "VI": "Virgin Islands, U.s.",
+    "WF": "Wallis and Futuna",
+    "EH": "Western Sahara",
+    "YE": "Yemen",
+    "ZM": "Zambia",
+    "ZW": "Zimbabwe"
+};
+*/
+
+
+function country_dropdown(user_country_code='') {
+    let option = '';
+    fetch('countries.json')
+    .then(response => {
+        return response.json();
+    })
+    .then(countries => countries)
+    
+
+    
+    for (let country_code in countries) {
+        let selected = (country_code == user_country_code) ? ' selected' : '';
+        let value = country_code;
+        let text = countries[country_code];
+        option += '<option value="'+value+'"'+selected+'>'+text+'</option>';
+    }
+    document.getElementById('countries').innerHTML = option;
+}
+// usage
+country_dropdown("NG"); // user country code for selected option
+
+
+
+function validate() {
+    
+    if( document.myForm.firstName.value == "" ) {
+       alert( "Please provide your First name!" );
+       document.myForm.Name.focus() ;
+       return false;
+    }
+    if( document.myForm.lastName.value == "" ) {
+        alert( "Please provide your Last name!" );
+        document.myForm.Name.focus() ;
+        return false;
+     }
+     if( document.myForm.userName.value == "" ) {
+        alert( "Please provide your  User name!" );
+        document.myForm.Name.focus() ;
+        return false;
+     }
+    if( document.myForm.email.value == "" ) {
+       alert( "Please provide your Email!" );
+       document.myForm.email.focus() ;
+       return false;
+    }
+    if( document.myForm.phoneNumber.value == "" || isNaN( document.myForm.phoneNumber.value ) ||
+       document.myForm.phoneNumber.value.length >11 ) {
+       
+       alert( "Please provide a valid phone number in the format 08068521406" );
+       document.myForm.phoneNumber.focus() ;
+       return false;
+    }
+    if( document.myForm.aboutSelf.value == "" || document.myForm.aboutSelf.value.length >100){
+        alert ( "Please tell us about yourself in no more than 100 characters");
+        document.myForm.aboutSelf.focus();
+        return false;
+    }
+
+     //check empty password field
+     if(document.getElementById("pwd1").value == "") {
+        document.getElementById("message1").innerHTML = "**Fill the password please!";
+       return false;
+     }
+    
+    //minimum password length validation 
+     if(document.myForm.password.value.length < 8) {
+        document.getElementById("message1").innerHTML = "**Password length must be atleast 8 characters";
+        return false;
+     }
+    //maximum length of password validation
+    if(document.myForm.password.value.length > 15) {
+       document.getElementById("message1").innerHTML = "**Password length must not exceed 15 characters";
+        return false;
+    }
+        //check empty confirm password field
+     if(document.getElementById("pwd2").value == "") {
+        document.getElementById("message2").innerHTML = "**Fill the password please!";
+        return false;
+    }
+
+        //comparing  pasword 1&2
+      var pw1 = document.getElementById("pwd1").value;  
+      var pw2 = document.getElementById("pwd2").value; 
+
+      if(pw1 != pw2) {  
+        document.getElementById("message2").innerHTML = "**Passwords are not same";  
+        return false;  
+      } else {  
+        prompt (" Enter the OTP sent to your phone number ." , " ") ;
+        alert ("Your account has been created successfully");
+        document.write("Your form has been submitted successfully, you may close your browser");
+      }
+ }
+
+
+let textArea = document.getElementById("aboutSelf");
+let characterCounter = document.getElementById("char-length");
+const maxNumOfChar = 100;
+
+
+const countCharacter = () => {
+    let numOfEnteredChars = textArea.value.length;
+    let counter = maxNumOfChar - numOfEnteredChars;
+    characterCounter.textContent = counter + "/100";
+}
+textArea.addEventListener("input", countCharacter);
+
+
+
